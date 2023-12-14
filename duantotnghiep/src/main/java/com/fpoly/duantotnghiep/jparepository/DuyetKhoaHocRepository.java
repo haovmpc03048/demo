4d8263b614696619f0dfb7a433aa1097f24af300
haovmpc03048 <133337015+haovmpc03048@users.noreply.github.com>
2023-12-14 23:02:43 +0700

    ok

--- duantotnghiep/src/main/java/com/fpoly/duantotnghiep/jparepository/DuyetKhoaHocRepository.java
+++
@@ -1,9 +0,0 @@
-package com.fpoly.duantotnghiep.jparepository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.fpoly.duantotnghiep.Entity.DuyetKhoaHoc;
-
-public interface DuyetKhoaHocRepository extends JpaRepository<DuyetKhoaHoc, Integer> {
-    // You can add custom query methods here if needed
-}
